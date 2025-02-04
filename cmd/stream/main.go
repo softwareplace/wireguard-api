4d5946e5c919b43546ef6bdf53f638257aac38f6
Elias Miereles <eliasmflilico@gmail.com>
2025-02-04 19:43:42 -0300

    Remove unused code and refactor API initialization
    Deleted commented-out JSON dump logic and removed unnecessary imports to clean up the code. Replaced `request.NewApi` with `request.NewService` for better clarity and functionality. These changes improve code maintainability and readability.

--- cmd/stream/main.go
+++ cmd/stream/main.go
@@ -6,7 +6,6 @@ import (
 	"github.com/softwareplace/http-utils/request"
 	"github.com/softwareplace/wireguard-api/cmd/stream/parse"
 	"github.com/softwareplace/wireguard-api/cmd/stream/spec"
-	"go/types"
 	"log"
 	"net/http"
 	"os"
@@ -33,21 +32,7 @@ func main() {
 		os.Exit(1)
 	}
 
-	//// Convert dump to JSON and print it
-	//jsonDump, err := json.MarshalIndent(dump, "", "  ")
-	//if err != nil {
-	//	log.Fatalf("Error converting dump to JSON: %v", err)
-	//}
-	//
-	//// Copy JSON dump to clipboard
-	//if err := exec.Command("bash", "-c", fmt.Sprintf("echo '%s' | xclip -selection clipboard", string(jsonDump))).Run(); err != nil {
-	//	log.Fatalf("Failed to copy JSON dump to clipboard: %v", err)
-	//}
-	//
-	//fmt.Println("Dump as JSON:")
-	//fmt.Println(string(jsonDump))
-
-	api := request.NewApi(types.Nil{})
+	api := request.NewService()
 	config := request.Build(streamEnv.Server).
 		WithPath("peers/stream").
 		WithHeader("Authorization", streamEnv.Authorization).
